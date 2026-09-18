5fe5722378edef77117148ac7cf32b97a37b236b
EmbryND <elvislimasamuel27@gmail.com>
2026-09-17 23:32:24 -0300

    Exercício de Vetor 1 - Corrigido!

--- src/LT02_VARHOMO1.java
+++ src/LT02_VARHOMO1.java
@@ -4,17 +4,24 @@ public class LT02_VARHOMO1 {
     public static void main (String args[]) {
 	int opc = 0;
 	int vetor[] = new int[5];
-	opc = Integer.parseInt(JOptionPane.showInputDialog("1 - Calcular média dos valores entre 10 e 200 \n 2 - Calcular soma dos ímpares \n 3 - Exibir resultados \n 9 - Sair"));
+        double media = 0;
+        int soma_impares = 0;
+        for (int i = 0; i < 50; i++) {
+            vetor[i] = Integer.parseInt(JOptionPane.showInputDialog("Digite o " +(i+1)+"º valor"));
+        }
 	while (opc != 9) {
+            opc = Integer.parseInt(JOptionPane.showInputDialog("1 - Calcular média dos valores entre 10 e 200 \n 2 - Calcular soma dos ímpares \n 3 - Exibir resultados \n 9 - Sair"));
             switch (opc) {
 		case 1:
-                    CalcularMedia(vetor);
+                    media = CalcularMedia(vetor);
+                    JOptionPane.showMessageDialog( null, "Média calculada!" );
                     break;
 		case 2:
-                    CalcularImpares(vetor);
+                    soma_impares = CalcularImpares(vetor);
+                    JOptionPane.showMessageDialog( null, "Soma dos ímpares calculada!" );
                     break;
 		case 3:
-                    ExibeResultados(vetor);
+                    ExibeResultados(media, soma_impares);
                     break;
 		case 9:
                     JOptionPane.showMessageDialog(null,"FIM");
@@ -24,28 +31,32 @@ public class LT02_VARHOMO1 {
         }		
     }
     public static double CalcularMedia(int vt[]) {
-        double media;
         int soma = 0;
-        for (int x = 0; x < 5; x++) {
-            vt[x] = Integer.parseInt(JOptionPane.showInputDialog("Digite um valor"));
-            System.out.println(x);
+        int cont = 0;
+        for (int x = 0; x < 50; x++) {
             if (vt[x] > 10 && vt[x] < 200) {
                 soma = soma + vt[x];
+                cont++;
             }
         }
-	return media = soma / 50;
+        if (cont > 0) { 
+            return (double) soma / cont; 
+        } else { 
+            return 0; 
+        }
     }
     
 	public static int CalcularImpares(int vt[]) {
 		int soma_impares = 0;
-		for (int i = 0; i < 5; i++) {
-			if (vt [i] % 1 == 0) {
+		for (int i = 0; i < 50; i++) {
+			if (vt [i] % 2 != 0) {
 				soma_impares = soma_impares + vt[i];
 			}
 		}
             return soma_impares;
 	}
-	public static int ExibeResultados(int vt[]) {
-		return 0;
+	public static int ExibeResultados(double media, int soma_impares) {
+            JOptionPane.showMessageDialog(null, "Média dos valores entre 10 e 200: " + media + "\nSoma dos números ímpares: " + soma_impares);
+            return 0;
 	}
 }
