b2e6c1643ee1b27cf7e6a5d831c049395a2465fe
osamuelelvis <elvislimasamuel27@gmail.com>
2026-09-22 18:32:35 -0300

    Correções

--- src/LT02_VARHOMO4.java
+++ src/LT02_VARHOMO4.java
@@ -5,19 +5,19 @@ public class LT02_VARHOMO4 {
 	double media;
 	int qtd_acima = 0;
 	double soma = 0;
-	for (int i = 0; i < 5; i++) {
+	for (int i = 0; i < 30; i++) {
 	    vetor[i] = Double.parseDouble(JOptionPane.showInputDialog("Digite o "+(i+1)+"º número"));
 	    soma = soma + vetor[i];
 	}
-	media = soma / 5.0;
-	for (int j = 0; j < 5; j++) {
+	media = soma / 30.0;
+	for (int j = 0; j < 30; j++) {
 	    if (vetor[j] > media) {
 		qtd_acima = qtd_acima + 1;
 	    }
 	}
 	System.out.println("Média do grupo: "+media+"\nQuantidade de notas acima da média: "+qtd_acima);
 	System.out.print("Posições das notas abaixo da média: ");
-	for (int k = 0; k < 5; k++) {
+	for (int k = 0; k < 30; k++) {
 	    if (vetor[k] < media) {
 		System.out.print(k+" ");
 	    }
