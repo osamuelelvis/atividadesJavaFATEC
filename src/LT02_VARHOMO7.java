fd2126d24dded21d8185923819ea3be0418e1a85
osamuelelvis <elvislimasamuel27@gmail.com>
2026-09-22 19:13:29 -0300

    Exercício 8 de Variáveis Homogêneas (Incompleto)

---
+++ src/LT02_VARHOMO7.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+public class LT02_VARHOMO7 {
+    public static void main (String args[]) {
+	int matriz[][] = new int[4][3];
+	for (int i = 0; i < 4; i++) {
+	    for (int j = 0; j < 3; j++) {
+		matriz[i][j] = Integer.parseInt(JOptionPane.showInputDialog("Digite a quantidade de produtos vendidos"));
+	    }
+	}
+    }
+}
